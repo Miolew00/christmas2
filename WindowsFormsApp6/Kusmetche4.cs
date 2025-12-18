dea4c0be79862a9c5dba8a17008292aeddfcae97
mirokolev <synzt59@gmail.com>
2025-12-18 16:32:49 +0200

    Add files via upload

---
+++ WindowsFormsApp6/Kusmetche4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Kusmetche4 : Form
+    {
+        public Kusmetche4()
+        {
+            InitializeComponent();
+        }
+    }
+}
